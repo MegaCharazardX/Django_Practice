9a31f06236c6f809e8cdfd7394c7489bd739b4a5
MegaCharazard <haridhejus@gmail.com>
2024-09-06 06:06:01 +0530

    Initial Commit

--- MyFirstDjango/MyFirstDjango/urls.py
+++ MyFirstDjango/MyFirstDjango/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("", include("calc.urls")),
+    path("", include("Travello.urls")),
     path('admin/', admin.site.urls),
 ]
 
--- MyFirstDjango/calc/views.py
+++ MyFirstDjango/calc/views.py
@@ -4,4 +4,11 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse("Hello World")
+    return render(request, "home.html", {"name" : "Hari"})
+
+def add(request):
+    
+    val1 = int(request.POST["num1"])
+    val2 = int(request.POST["num2"])
+    res = val1 + val2
+    return render(request, "result.html", {"result" : res})
